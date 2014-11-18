861ec204ea22cb6a57f8550efb059114e18c5dd9
Martin Hill <martin@hillm.net>
2014-11-18 16:10:47 -0500

    removed permissions stuff

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.models import User
-from rest_framework import generics, permissions
+from rest_framework import generics
 from authentication.serializers import UserSerializer
 
 
 class UserCreateView(generics.CreateAPIView):
-    # permission_classes = (permissions.AllowAny,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
